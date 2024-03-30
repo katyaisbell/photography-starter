0a99356819a4a055adc822155e47d57afd977cf9
Katya Isbell <katyaisdev@gmail.com>
2024-03-29 19:20:07 -0600

    change config size

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       // secondary: "Sometype Mono",
     },
     screens: {
-      sm: "390px",
+      sm: "490px",
       md: "768px",
       lg: "1024px",
       xl: "1192px",
