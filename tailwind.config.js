603b996eca12e71e811252072dab73b0b23a8d90
Katya Isbell <katyaisdev@gmail.com>
2024-03-29 21:51:09 -0600

    screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       // secondary: "Sometype Mono",
     },
     screens: {
-      sm: "300px",
+      sm: "370px",
       md: "768px",
       lg: "1024px",
       xl: "1192px",
