7973dec790c0149535eb800ac870671c7787a62e
Katya Isbell <katyaisdev@gmail.com>
2024-03-29 21:47:55 -0600

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ module.exports = {
       // secondary: "Sometype Mono",
     },
     screens: {
-      sm: "490px",
+      sm: "300px",
       md: "768px",
       lg: "1024px",
       xl: "1192px",
