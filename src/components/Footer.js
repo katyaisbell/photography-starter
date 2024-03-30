67673fa7bcd2c1041edbe337566e8add35ff864f
Katya Isbell <katyaisdev@gmail.com>
2024-03-29 21:43:22 -0600

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center bg-green">
+    <footer className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center ">
       <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
         <p>Design + Code by Katya Isbell </p>
       </div>
